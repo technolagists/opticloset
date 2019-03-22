4102f3c0f8e1942ccce102b6864fc22ad2e973cb
lauraannpena <lauraannpena@gmail.com>
2019-03-21 19:58:26 -0500

    add default if coordinates are not provided

--- apiHelpers/openWeatherApi.js
+++ apiHelpers/openWeatherApi.js
@@ -2,19 +2,38 @@ require('dotenv').config();
 const axios = require('axios');
 
 module.exports.getWeather = (latLong) => {
-  return axios
-    .get(
-      `http://api.openweathermap.org/data/2.5/weather?lat=${latLong.latitude}&lon=${latLong.longitude}&units=imperial&APPID=${process.env.OPEN_WEATHER_API_KEY}`,
-    )
-    .then((response) => {
-      const result = {
-        temp: response.data.main.temp,
-        weather: response.data.weather[0].description,
-      };
-      return result;
-    })
-    .catch((error) => {
-      console.log(error);
-      return error;
-    });
+  if (!latLong.latitude) {
+    return axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/forecast?q=New%20Orleans,%20US&units=imperial&APPID=${process.env.OPEN_WEATHER_API_KEY}`,
+      )
+      .then((response) => {
+        const result = {
+          temp: response.data.list[1].main.temp,
+          weather: response.data.list[1].weather[0].description,
+        };
+        return result;
+      })
+      .catch((error) => {
+        console.log(error);
+        return error;
+      });
+  }
+  if (latLong.latitude) {
+    return axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?lat=${latLong.latitude}&lon=${latLong.longitude}&units=imperial&APPID=${process.env.OPEN_WEATHER_API_KEY}`,
+      )
+      .then((response) => {
+        const result = {
+          temp: response.data.main.temp,
+          weather: response.data.weather[0].description,
+        };
+        return result;
+      })
+      .catch((error) => {
+        console.log(error);
+        return error;
+      });
+  }
 };
