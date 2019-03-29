f5529a97746ad905bcc3f6625dd6b591b4795f08
Julien de la Mettrie <jdelamettrie@gmail.com>
2019-03-28 19:36:38 -0500

    '/location' endpoint now functionalv=> sending back formatted address

--- apiHelpers/googleMapsApi.js
+++ apiHelpers/googleMapsApi.js
@@ -5,14 +5,14 @@ const axios = require('axios');
 module.exports.getLocation = (latLong) => {
   return axios.get(
     `https://maps.googleapis.com/maps/api/geocode/json?latlng=
-      ${latLong.latitude},${latLong.longitude}40.714224,-73.961452&key=${process.env.GOOGLE_GEOCODING_API_KEY}`,
+      ${latLong}&key=${process.env.GOOGLE_GEOCODING_API_KEY}`,
   )
     .then((response) => {
       // const result = {
       //   temp: response.data.list[1].main.temp,
       //   weather: response.data.list[1].weather[0].description
       // };
-      return response;
+      return response.data.results[0].formatted_address;
     })
     .catch((error) => {
       console.log(error);
