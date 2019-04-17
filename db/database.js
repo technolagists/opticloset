5242368b52589767440460903b239ad32e5f022a
Julien de la Mettrie <jdelamettrie@gmail.com>
2019-04-17 15:22:37 -0500

    5 tests now functional for server.spec

--- db/database.js
+++ db/database.js
@@ -5,6 +5,7 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
+  logging: false,
   dialect: 'postgres',
   pool: {
     max: 5,
